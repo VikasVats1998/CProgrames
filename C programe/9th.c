98d5351c31bd122c2446e52678521509c0ad50d7
Vikas <vvats888@gmail.com>
2021-09-16 06:49:48 +0530

    2nd commit

--- C programe/9th.c
+++ C programe/9th.c
@@ -5,7 +5,11 @@ int main()
     printf("Enter two no.\n");
     scanf("%d%d",&a,&b);
     if((a>=20&&a<50)||(b>=20&&b<=50))
-        printf("true");
+        {
+            printf("true");
+        }
     else
-        printf("false");
+        {
+            printf("false");
+        }
 }
