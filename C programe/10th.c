30fbf2aa4920dd13d432b51dff6c7e03d70f3110
Vikas <vvats888@gmail.com>
2021-09-16 06:31:27 +0530

    initial commit

---
+++ C programe/10th.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,x,y;
+    printf("Enter two no.");
+    scanf("%d%d",&a,&b);
+    x=abs(100-a);
+    y=abs(100-b);
+    if(x<y){
+        printf("%d",a);
+    }
+    else if(x=y){
+        printf("0");
+    }
+    else{
+        printf("%d",b);
+    }
+    return 0;
+}
