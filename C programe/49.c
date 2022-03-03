fba6ceec9836c3bfca37e5a47eb3ed643303a347
VikasVats1998 <vvats888@gmail.com>
2022-03-03 12:30:59 +0530

    update

--- C programe/49.c
+++ C programe/49.c
@@ -1,24 +1,38 @@
 #include<stdio.h>
-int main(){
-    int array[100],i,n;
-    int arr_size1;
-    arr_size1 = sizeof(array)/sizeof(array[0]);
-    printf("Enter the size of array : ");
+int main()
+{
+    int arr[100],new[100], i,n;
+    printf("Enter the number of elements:n");
     scanf("%d",&n);
-    printf("Enter the array elements\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&array[i]);
-    }
-    printf("\nYour array\n");
-    for ( i = 0; i < n; i++)
+    printf("\nEnter the array elements\n");
+    for(i=0;i<n;i++)
     {
-        printf("%4d",array[i]);
+        scanf("%d",&arr[i]);
     }
-     int new[] = { array[arr_size1 / 2 - 1], array[arr_size1 / 2], array[arr_size1 / 2 + 1]} ;
-     printf("\nNew array\n");
-     for(i=0;i<3;i++){
-         printf("%d",new[i]);
-     }
-
+    if(n%2==0)
+    {
+        n=n/2;
+        for(i=0;i<3;i++)
+        {
+          new[i]=arr[n-1];
+          n++;  
+        }
+    }
+    else
+    {   
+        n+1;
+        n=n/2;
+        for(i=0;i<3;i++)
+        {
+            new[i]=arr[n];
+            n++;
+        }
+    }
+    printf("\nNew array :");
+    for(i=0;i<3;i++)
+    {
+        printf("%d",new[i]);
+    }
     
+
 }
--- C programe/53.c
+++ C programe/53.c
@@ -1,4 +1,70 @@
 #include<stdio.h>
 int main(){
-    
+    int array[100],n,i,count=0,sum=0;
+    printf("Enter the size of array :");
+    scanf("%d",&n);
+    printf("\nEnter the array elements \n");
+    for(i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    for(i=0;i<n;i++){
+        if (array[i] == 5) count = 1;
+        else if (count && array[i] == 6) count = 0;
+        else if (!count) sum += array[i];
+    }
+    printf("Sum is : %d",sum);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(void){
+//     int array1[] = {2, 5, 9, 10, 17};
+//     int arr_size = sizeof(array1)/sizeof(array1[0]);
+//     printf("Sum of values in the array of integers except those numbers starting with 5 followed by atleast one 6: ");
+//     printf("%d",test(array1, arr_size));
+//   }    
+//   int test(int nums[], int arr_size)
+//          {
+//             int sum = 0;
+//             int inSection = 0;
+//             for (int i = 0; i < arr_size; i++)
+//             {
+//                 if (nums[i] == 5) inSection = 1;
+//                 else if (inSection && nums[i] == 6) inSection = 0;
+//                 else if (!inSection) sum += nums[i];
+//             }
+//             return sum;
+//          }
